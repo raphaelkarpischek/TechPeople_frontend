c637bd2227007b98905185a36e0dd51836a05cec
raphael.martinez <raphael.martinez@ex.gerdau.com>
2022-11-22 10:07:30 -0300

    comment with API baseURL instruction

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://techpeople-backend.herokuapp.com/'
+    baseURL: 'https://techpeople-backend.herokuapp.com/' //ou http://localhost:5000/
 })
